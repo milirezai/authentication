63cad38742bbcdc3d2ee8a65b3df26e1a0be3b29
milad rezai <miladrezaix@gmail.com>
2025-06-03 11:17:27 +0330

    Completion of validation class

--- classes/class/validation.php
+++ classes/class/validation.php
@@ -1,51 +1,93 @@
 <?php
 class validation{
-    public $validUserName;
-    public $validEmail;
-    public $validMObileNumber;
-    public $validPassword;
+ private $validation;
 
     public function validUserName($userName){
+if (!empty($userName)) {
 
-        if (is_string($userName)) {
+    if (is_string($userName)) {
 
-            if (strlen($userName) > 2) {
-                return $this->validUserName=1;
-             }else {
-                return $this->validUserName=0;
-             }
+        if (strlen($userName) > 2) {
+            return $this->validation=true;
+            // return "نام اوکی است";
+          }else {
+            return $this->validation=false;
+            // return "طول نام کوتاه است";
+          }
 
-        } else {
-            return $this->validUserName=0;
-         }
+    } else {
+        return $this->validation=false;
+        // return "نوع نام رشته نیست";
+      }
 
+
+ }else {
+    return $this->validation=false;
+    // return "نام خالی است !";
+}
     }
 
     public function validEmail($email){
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-          return   $this->validEmail=1;
-         } else {
-            return  $this->validEmail=0;
-         }
+        if (!empty($email)) {
+
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                return   $this->validation=true;
+                // return   "ایمیل اوکی است";
+               } else {
+                  return  $this->validation=false;
+                //   return  "ایمیل نا معتبر است";
+               }
+      
+        }else {
+           return $this->validation=false;
+        //    return "ایمیل خالی است";
+        }
+        
     }
 
     public function validMObileNumber($mobileNumber){
-        if (is_numeric($mobileNumber)) {
-            if (  strlen($mobileNumber) == 11 ) {
-                return  $this->validMObileNumber=1;
-            }else {
-                return  $this->validMObileNumber=0;
-            }
-            return  $this->validMObileNumber=1;
-        } else {
-            return  $this->validMObileNumber=0;
+        if (!empty($mobileNumber)) {
+
+            if (is_numeric($mobileNumber)) {
+
+                if (  strlen($mobileNumber) == 11 ) {
+                    return  $this->validation=true;
+                    // return  "موبایل اوکی است";
+                 }elseif (strlen($mobileNumber) > 11) {
+                    return  $this->validation=false;
+                    // return  "تعداد عدد های موبایل بیشتر است";
+                 }else {
+                    return  $this->validation=false;
+                    // return  "تعداد عدد های موبایل کم است";
+                 }
+
+             } else {
+                return  $this->validation=false;
+                // return "فرمت موبایل عددی نیست";
+             }
+    
+        }else {
+           return $this->validation=false;
+        //    return "موبایل خالی است";
         }
+        
     }
 
-    public function validPassword(){
+    public function validPassword($password){
+        if (!empty($password)) {
+            if (strlen($password) >= 8) {
+                return $this->validation=true;
+                // return "پسورد اوکی است";
+            }else {
+                return $this->validation=false;
+                // return "طول پسورد کم تر از ۸ است";
+             }
+        }else {
+           return $this->validation=false;
+        //    return "پسورد خالی است";
+        }
+        
 
     }
 }
-$user= new validation();
-echo $user->validMObileNumber('09167516826');
   
